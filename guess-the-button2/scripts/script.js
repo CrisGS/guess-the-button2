8934d1dedf034bcda0d0e365ddbbf96560088fa8
CrisGS <93886227+CrisGS@users.noreply.github.com>
2022-03-27 23:36:02 +0300

    Update script.js

--- guess-the-button2/scripts/script.js
+++ guess-the-button2/scripts/script.js
@@ -1,15 +1,25 @@
 let winnerButton;
 
 function addButtons() {
+  let addedButtons = 0;
   let buttonsNumber = document.getElementById("btnsNumber").valueAsNumber;
   let winNumber = Math.floor((Math.random() * buttonsNumber) + 1);
-  for (let i = 1; i <= buttonsNumber; ++i) {
-    let myDiv = document.createElement("div");
-    myDiv.id = 'buttons-place';
-    myDiv.innerHTML = "<button id='"+ i +"' title='Are you sure?' onclick='clickedButton(this.id), checkWinOrLose(clickedButton(this.id))'>Click me!</button>";
-    document.body.appendChild(myDiv);
+  if (buttonsNumber > 2) {
+    for (let i = 1; i <= buttonsNumber; ++i) {
+      let myDiv = document.createElement("div");
+      myDiv.id = 'buttons-place';
+      myDiv.innerHTML = "<button id='"+ i +"' title='Are you sure?' onclick='clickedButton(this.id), checkWinOrLose(clickedButton(this.id))'>Click me!</button>";
+      document.body.appendChild(myDiv);
+      ++addedButtons;
+    }
+  } else {
+    alert ("Please, insert a number greater than 2!");
+    location.reload();
   }
   winnerButton = generateWinNumber(buttonsNumber);
+  if (addedButtons === buttonsNumber) {
+    document.getElementById("button").disabled = true;
+  }
 }
 
 function generateWinNumber (randomNumber) {
@@ -24,7 +34,8 @@ function clickedButton(myBtn) {
 
 function checkWinOrLose(clickedButton) {
   if (winnerButton == clickedButton) {
-    alert("BooYaaah! You chose the right one :) Congratulations! You are the champion!");
+    alert("BooYaaah! You chose the right one :) Congratulations! You are the champion! Wanna play again? Just hit the 'OK' button and the game will restart :)");
+    location.reload();
   } else {
     alert ("Oops! You push the wrong button! Maybe the next choice will be the right one");
   }
